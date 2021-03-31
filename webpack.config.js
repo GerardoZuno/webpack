11bee0008fd13b8c2413e7a4583263c133ac8992
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-03-31 14:48:38 -0600

    Html added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -20,4 +20,11 @@ module.exports = {
         }
     ]
 },
+    plugins: [
+        new  HtmlWebpackPlugin({
+            inject: true, 
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
 }
